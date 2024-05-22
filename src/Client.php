feaff36ef4f49818076534c1736ced0dde9d3cda
Vaishali Jagtap <71736051+vaishali-vumc@users.noreply.github.com>
2024-05-22 15:53:28 -0400

    debug

--- src/Client.php
+++ src/Client.php
@@ -91,6 +91,7 @@ class Client implements ClientInterface
                 $messageArr = json_decode(json_encode($param), true);
                 unset($messageArr['message']['registration_ids']);
                 $messageArr['message']['topic'] = $topic;
+                echo json_encode($messageArr);
                 $output = $this->guzzleClient->post(
                                     $this->getHTTPV1ApiUrl(),
                                     [
@@ -105,8 +106,6 @@ class Client implements ClientInterface
             $response = $this->removeTopicSubscription($topic, $tokens);
             return $output;
         } else {
-            print_r($param);
-            echo json_encode($param); die;
             return $this->guzzleClient->post(
                 $this->getHTTPV1ApiUrl(),
                 [
